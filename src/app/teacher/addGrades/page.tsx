4976e9ca5f2098928fea12e2bc6969f5e6781e87
Farhin Rahman <farhin.rahmanp@gmail.com>
2024-04-20 01:01:42 +0600

    add grades

--- src/app/teacher/addGrades/page.tsx
+++ src/app/teacher/addGrades/page.tsx
@@ -6,58 +6,94 @@ export default function addGrades() {
     <div className="flex flex-row gap-x-44 bg-white">
       <Sidebar />
 
-      <section className="flex flex-col w-2/4 gap-y-10">
-        <div className="flex flex-col gap-y-4 p-4">
-          <div className="flex flex-row justify-between items-center">
-            <div className="flex flex-row justify-between items-center bg-white p-4 mt-16">
-              <h1 className="text-2xl font-bold font-serif">Add Grades</h1>
-            </div>
-          </div>
-
-          <form className="flex flex-col gap-y-2">
-            <div className="flex flex-col gap-y-1">
-              <div className="flex flex-row justify-between items-center bg-black text-white p-4">
-                <h1 className="text-md font-serif">Roll No</h1>
-                <h1 className="text-md font-serif">Name</h1>
-                <h1 className="text-md font-serif">Grades</h1>
-                <h1 className="text-md font-serif">Marks</h1>
-              </div>
-              <div className="flex flex-col gap-y-1">
-                <div className="flex flex-row justify-between items-center bg-gray-100 p-1">
-                  <h1 className="text-md font-serif">1.</h1>
-                  <h1 className="text-md font-serif">Sifat Jahan</h1>
-                  <h1 className="text-md font-serif">A+</h1>
-                  <h1 className="text-md font-serif">90</h1>
-                </div>
-                <div className="flex flex-row justify-between items-center bg-gray-100 p-1">
-                  <h1 className="text-md font-serif">2.</h1>
-                  <h1 className="text-md font-serif">Tanvirul Islam</h1>
-                  <h1 className="text-md font-serif">A</h1>
-                  <h1 className="text-md font-serif">85</h1>
-                </div>
-                <div className="flex flex-row justify-between items-center bg-gray-100 p-1">
-                  <h1 className="text-md font-serif">3.</h1>
-                  <h1 className="text-md font-serif">Farhin Rahman</h1>
-                  <h1 className="text-md font-serif">A+</h1>
-                  <h1 className="text-md font-serif">90</h1>
-                </div>
-                <div className="flex flex-row justify-between items-center bg-gray-100 p-1">
-                  <h1 className="text-md font-serif">4.</h1>
-                  <h1 className="text-md font-serif">Farhana Chowdhury</h1>
-                  <h1 className="text-md font-serif">A</h1>
-                  <h1 className="text-md font-serif">80</h1>
-                </div>
-                <div className="flex flex-row justify-between items-center bg-gray-100 p-1">
-                  <h1 className="text-md font-serif">5.</h1>
-                  <h1 className="text-md font-serif">Tashin Ahmed</h1>
-                  <h1 className="text-md font-serif">A+</h1>
-                  <h1 className="text-md font-serif">95</h1>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
-      </section>
+      <div className="max-w-4xl bg-white p-5 rounded-md -ml-14">
+        <h1 className="text-2xl font-bold mb-4">Student Grade Distribution</h1>
+        <table className="w-full mb-6">
+          <thead>
+            <tr>
+              <th className="px-2 py-2 bg-black text-white">Roll Number</th>
+              <th className="px-2 py-2 bg-black text-white">Name</th>
+              <th className="px-2 py-2 bg-black text-white">Marks</th>
+              <th className="px-2 py-2 bg-black text-white">Grade</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border px-2 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="number" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="number" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="number" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="number" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+            </tr>
+            <tr>
+              <td className="border px-2 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="number" className="focus:outline-none" />
+              </td>
+              <td className="border px-4 py-2">
+                <input type="text" className="focus:outline-none" />
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <button className="bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          Submit
+        </button>
+      </div>
     </div>
   );
 }
